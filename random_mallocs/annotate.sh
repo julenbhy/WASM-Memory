0274b257e17beadbfc19e44e2ca78ec3f7c14c36
julenbhy <julenbhy@example.com>
2023-06-02 12:02:01 +0200

    update

--- random_mallocs/annotate.sh
+++
@@ -1,5 +0,0 @@
-sudo perf record -k mono /home/julen/.wasmtime/bin/wasmtime --profile=jitdump clang.wasm
-sudo perf inject --jit --input perf.data --output perf.jit.data
-sudo perf annotate --input perf.jit.data
-
-rm -f *.wasm *.so *.data *.dump
